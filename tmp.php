2c984449679073d52a027d16e7bb9f8cae57d2a9
pagnoul-marvin <marvin.pagnoul@student.hepl.be>
2024-02-26 08:33:04 +0100

    Added files to jiris

--- tmp.php
+++ tmp.php
@@ -1,6 +1,8 @@
 <?php
-$jiris = [['id'=>1,'name'=>'Projets Web 2025', 'starting_at' => DateTimeImmutable::createFromFormat('j-M-y', "15-Jan-2025")],
-    ['id'=>2,'name'=>'Design Web 2024', 'starting_at' => DateTimeImmutable::createFromFormat('j-M-y', "15-Jan-2024")],
-    ['id'=>3,'name'=>'Projets Web 2024', 'starting_at' => DateTimeImmutable::createFromFormat('j-M-y', "15-Jan-2024")],
-    ['id'=>4,'name'=>'Design Web 2023', 'starting_at' => DateTimeImmutable::createFromFormat('j-M-y', "15-Jan-2023")]];
-?>
+$jiris = [
+    ['id'=>1,'name'=>'Projets Web 2025', 'starting_at' => '2025-06-02 08:58:08'],
+    ['id'=>2,'name'=>'Design Web 2024', 'starting_at' => '2024-06-02 08:58:08'],
+    ['id'=>3,'name'=>'Projets Web 2024', 'starting_at' => '2024-06-02 08:58:08'],
+    ['id'=>4,'name'=>'Design Web 2023', 'starting_at' => '2023-06-02 08:58:08']
+];
+
